fff76aa181a6bed0faaeda13b54681a39c3c3bb0
Matteo Pierro <pierromatteo@gmail.com>
2020-12-01 14:28:46 +0200

    find combinations

--- test/first_day_test.rb
+++ test/first_day_test.rb
@@ -6,6 +6,10 @@ require_relative '../lib/first_day'
 class FirstDayTest < Minitest::Test
   def test_parses_line_files
     path = './test/test_file'
-    assert_equal %w{1721 979 366 299 675 1456}, FirstDay.file_lines(path).to_a
+    assert_equal %w[1721 979 366 299 675 1456], FirstDay.file_lines(path).to_a
+  end
+
+  def test_permutations
+    assert_equal([[1, 2], [1, 3], [2, 3]], FirstDay.combinations(array: [1, 2, 3], size: 2).to_a)
   end
 end
