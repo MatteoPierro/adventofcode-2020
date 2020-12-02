7e330e3777541ea2c6a43f6b03dc5332e7461196
Matteo Pierro <pierromatteo@gmail.com>
2020-12-02 12:53:51 +0200

    solve second puzzle day2

--- lib/password_philosophy.rb
+++ lib/password_philosophy.rb
@@ -5,18 +5,34 @@ require_relative 'file_helper'
 # Solution for https://adventofcode.com/2020/day/2
 
 class PasswordPhilosophy
-  def self.count_compliant(passwords_with_policies = nil)
-    passwords_with_policies ||= File.readlines('./lib/password_philosophy.txt')
-    passwords_with_policies.select(&method(:compliant?)).count
-  end
-
-  def self.compliant?(password_with_policy)
-    match = password_with_policy.match(/^(?<min>\d+)-(?<max>\d+)\s(?<letter>\w):\s(?<word>\w+)$/)
-    min = match[:min].to_i
-    max = match[:max].to_i
-    letter = match[:letter]
-    word = match[:word]
+  COUNT_POLICY = lambda do |min, max, letter, word|
     occurrences = word.count(letter)
     occurrences >= min && occurrences <= max
+  end
+
+  EXACTLY_ONE_POLICY = lambda do |first_position, second_position, letter, word|
+    return false if word[first_position - 1] == letter && word[second_position - 1] == letter
+
+    word[first_position - 1] == letter || word[second_position - 1] == letter
+  end
+
+  attr_reader :policy
+
+  def initialize(policy = nil)
+    @policy = policy || COUNT_POLICY
+  end
+
+  def count_compliant(password_with_constraints = nil)
+    password_with_constraints ||= File.readlines('./lib/password_philosophy.txt')
+    password_with_constraints.select(&method(:compliant?)).count
+  end
+
+  def compliant?(password_with_constraints)
+    match = password_with_constraints.match(/^(?<first>\d+)-(?<second>\d+)\s(?<letter>\w):\s(?<word>\w+)$/)
+    first = match[:first].to_i
+    second = match[:second].to_i
+    letter = match[:letter]
+    word = match[:word]
+    policy.call(first, second, letter, word)
   end
 end
--- test/password_philosophy_test.rb
+++ test/password_philosophy_test.rb
@@ -5,19 +5,29 @@ require_relative '../lib/password_philosophy'
 
 class PasswordPhilosophyTest < Minitest::Test
   def test_compliant_password_to_policy
-    assert PasswordPhilosophy.compliant?('1-3 a: abcde')
+    assert PasswordPhilosophy.new.compliant?('1-3 a: abcde')
   end
 
   def test_not_compliant_password_to_policy
-    assert !PasswordPhilosophy.compliant?('1-3 b: cdefg')
+    assert !PasswordPhilosophy.new.compliant?('1-3 b: cdefg')
   end
 
   def test_count_compliant_password_to_policy
-    passwords_with_policies = ['1-3 a: abcde', '1-3 b: cdefg', '2-9 c: ccccccccc']
-    assert_equal 2, PasswordPhilosophy.count_compliant(passwords_with_policies)
+    password_with_constraints = ['1-3 a: abcde', '1-3 b: cdefg', '2-9 c: ccccccccc']
+    assert_equal 2, PasswordPhilosophy.new.count_compliant(password_with_constraints)
   end
 
   def test_solution_first_puzzle
-    assert_equal 620, PasswordPhilosophy.count_compliant
+    assert_equal 620, PasswordPhilosophy.new.count_compliant
+  end
+
+  def test_not_compliant_password_to_exactly_one_policy
+    policy = PasswordPhilosophy::EXACTLY_ONE_POLICY
+    assert !PasswordPhilosophy.new(policy).compliant?('2-9 c: ccccccccc')
+  end
+
+  def test_solution_second_puzzle
+    policy = PasswordPhilosophy::EXACTLY_ONE_POLICY
+    assert_equal 727, PasswordPhilosophy.new(policy).count_compliant
   end
 end
