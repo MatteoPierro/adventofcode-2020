5f756c2e87f15a7f4323f105203d7405cd9865a1
Matteo Pierro <pierromatteo@gmail.com>
2020-12-03 17:52:56 +0200

    solve second puzzle day3

--- lib/toboggan_trajectory.rb
+++ lib/toboggan_trajectory.rb
@@ -4,14 +4,28 @@ require_relative 'file_helper'
 
 # Solution for https://adventofcode.com/2020/day/1
 
-module TobogganTrajectory
-  class << self
-    def count_trees(area = nil)
-      area ||= File.readlines('./lib/toboggan_trajectory.txt')
-      area.each.map.with_index do |row, index|
-        position = (index * 3) % row.length
-        row[position]
-      end.count('#')
-    end
+class TobogganTrajectory
+  attr_reader :area
+
+  def initialize(area = nil)
+    @area = area || File.readlines('./lib/toboggan_trajectory.txt')
+  end
+
+  def multiply_trees
+    [
+      { right: 1, down: 1 },
+      { right: 3, down: 1 },
+      { right: 5, down: 1 },
+      { right: 7, down: 1 },
+      { right: 1, down: 2 }
+    ].map { |slope| count_trees(**slope) }.reduce(:*)
+  end
+
+  def count_trees(right: 3, down: 1)
+    (0...area.length)
+      .step(down)
+      .map { |index| area[index] }
+      .map.with_index { |row, index| row[(index * right) % row.length] }
+      .count('#')
   end
 end
--- test/toboggan_trajectory_test.rb
+++ test/toboggan_trajectory_test.rb
@@ -9,10 +9,34 @@ class TobogganTrajectoryTest < Minitest::Test
       ..#
       #..
     ]
-    assert_equal(1, TobogganTrajectory.count_trees(area))
+    assert_equal(1, TobogganTrajectory.new(area).count_trees)
   end
 
   def test_first_puzzle_solution
-    assert_equal(272, TobogganTrajectory.count_trees)
+    assert_equal(272, TobogganTrajectory.new.count_trees)
+  end
+
+  def test_one_x_one_area_with_different_combination
+    area = %w[
+      .
+      #
+    ]
+    toboggan_trajectory = TobogganTrajectory.new(area)
+    assert_equal(1, toboggan_trajectory.count_trees(right: 2, down: 1))
+  end
+
+  def test_multiply_trees
+    area = File.readlines('./test/toboggan_trajectory_test.txt')
+    toboggan_trajectory = TobogganTrajectory.new(area)
+    assert_equal(2, toboggan_trajectory.count_trees(right: 1, down: 1))
+    assert_equal(7, toboggan_trajectory.count_trees(right: 3, down: 1))
+    assert_equal(3, toboggan_trajectory.count_trees(right: 5, down: 1))
+    assert_equal(4, toboggan_trajectory.count_trees(right: 7, down: 1))
+    assert_equal(2, toboggan_trajectory.count_trees(right: 1, down: 2))
+    assert_equal(336, toboggan_trajectory.multiply_trees)
+  end
+
+  def test_second_puzzle_solution
+    assert_equal(3_898_725_600, TobogganTrajectory.new.multiply_trees)
   end
 end
