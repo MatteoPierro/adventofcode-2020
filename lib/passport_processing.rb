887bc102f3abe2390179e8e44cdc3fbf8d4dea4f
Matteo Pierro <pierromatteo@gmail.com>
2020-12-05 01:44:28 +0200

    extract Validator module

--- lib/passport_processing.rb
+++ lib/passport_processing.rb
@@ -3,58 +3,8 @@
 require_relative './file_helper'
 
 module PassportProcessing
-  MANDATORY_FIELDS = %w[byr iyr eyr hgt hcl ecl pid].freeze
-
-  SIMPLE_VALIDATOR = lambda do |passport|
-    (MANDATORY_FIELDS - passport.scan(/(\w+):\S+/).flatten).empty?
-  end
-
-  COMPLETE_VALIDATOR = lambda do |raw_passport|
-    return false unless SIMPLE_VALIDATOR.call(raw_passport)
-
-    passport = raw_passport.scan(/(\w+):(\S+)/).each_with_object({}) do |field, p|
-      p[field.first.to_sym] = field.last
-    end
-
-    return validate_range(passport[:byr], 1920, 2002) &&
-           validate_range(passport[:iyr], 2010, 2020) &&
-           validate_range(passport[:eyr], 2020, 2030) &&
-           validate_height(passport[:hgt]) &&
-           validate_hair_color(passport[:hcl]) &&
-           validate_eye_color(passport[:ecl]) &&
-           validate_passport_id(passport[:pid])
-  end
-
-  def self.validate_passport_id(passport_id)
-    passport_id.match?(/^\d{9}$/)
-  end
-
-  def self.validate_eye_color(eye_color)
-    %w[amb blu brn gry grn hzl oth].include? eye_color
-  end
-
-  def self.validate_hair_color(raw_hair_color)
-    raw_hair_color.match?(/^#[0-9a-f]{6}$/)
-  end
-
-  def self.validate_height(raw_height)
-    height_parts = raw_height.scan(/(\d+)(cm|in)/).first
-    return false if height_parts.nil?
-
-    (value, scale) = height_parts
-    if scale == 'cm'
-      validate_range(value, 150, 193)
-    else
-      validate_range(value, 59, 76)
-    end
-  end
-
-  def self.validate_range(value, lower_value, higher_value)
-    (lower_value..higher_value).include? value.to_i
-  end
-
   class << self
-    def count_valid_passports(passport_filepath: nil, validator: SIMPLE_VALIDATOR)
+    def count_valid_passports(passport_filepath: nil, validator: Validator::SIMPLE_VALIDATOR)
       passport_filepath ||= './lib/passport_processing.txt'
       parse(passport_filepath)
         .filter { |passport| validator.call(passport) }
@@ -66,7 +16,60 @@ module PassportProcessing
           .chunk(&:empty?)
           .map { |_, v| v.join(' ') }
           .reject(&:empty?)
-          .to_a
+    end
+  end
+
+  module Validator
+    MANDATORY_FIELDS = %w[byr iyr eyr hgt hcl ecl pid].freeze
+
+    SIMPLE_VALIDATOR = lambda do |passport|
+      (MANDATORY_FIELDS - passport.scan(/(\w+):\S+/).flatten).empty?
+    end
+
+    COMPLETE_VALIDATOR = lambda do |raw_passport|
+      return false unless SIMPLE_VALIDATOR.call(raw_passport)
+
+      passport = raw_passport.scan(/(\w+):(\S+)/).each_with_object({}) do |field, p|
+        p[field.first.to_sym] = field.last
+      end
+
+      return validate_range(passport[:byr], 1920, 2002) &&
+             validate_range(passport[:iyr], 2010, 2020) &&
+             validate_range(passport[:eyr], 2020, 2030) &&
+             validate_height(passport[:hgt]) &&
+             validate_hair_color(passport[:hcl]) &&
+             validate_eye_color(passport[:ecl]) &&
+             validate_passport_id(passport[:pid])
+    end
+
+    class << self
+      def validate_passport_id(passport_id)
+        passport_id.match?(/^\d{9}$/)
+      end
+
+      def validate_eye_color(eye_color)
+        %w[amb blu brn gry grn hzl oth].include? eye_color
+      end
+
+      def validate_hair_color(raw_hair_color)
+        raw_hair_color.match?(/^#[0-9a-f]{6}$/)
+      end
+
+      def validate_height(raw_height)
+        height_parts = raw_height.scan(/(\d+)(cm|in)/).first
+        return false if height_parts.nil?
+
+        (value, scale) = height_parts
+        if scale == 'cm'
+          validate_range(value, 150, 193)
+        else
+          validate_range(value, 59, 76)
+        end
+      end
+
+      def validate_range(value, lower_value, higher_value)
+        (lower_value..higher_value).include? value.to_i
+      end
     end
   end
 end
--- test/passport_processing_test.rb
+++ test/passport_processing_test.rb
@@ -6,17 +6,17 @@ require_relative '../lib/passport_processing'
 class PassportProcessingTest < Minitest::Test
   def test_valid_passport
     valid_passport = 'ecl:gry pid:860033327 eyr:2020 hcl:#fffffd byr:1937 iyr:2017 cid:147 hgt:183cm'
-    assert PassportProcessing::SIMPLE_VALIDATOR.call(valid_passport)
+    assert PassportProcessing::Validator::SIMPLE_VALIDATOR.call(valid_passport)
   end
 
   def test_invalid_passport
     invalid_passport = 'pid:860033327 eyr:2020 hcl:#fffffd byr:1937 iyr:2017 cid:147 hgt:183cm'
-    assert_equal PassportProcessing::SIMPLE_VALIDATOR.call(invalid_passport), false
+    assert_equal PassportProcessing::Validator::SIMPLE_VALIDATOR.call(invalid_passport), false
   end
 
   def test_still_valid_passport_with_missing_cid
     valid_passport = 'ecl:gry pid:860033327 eyr:2020 hcl:#fffffd byr:1937 iyr:2017 hgt:183cm'
-    assert PassportProcessing::SIMPLE_VALIDATOR.call(valid_passport)
+    assert PassportProcessing::Validator::SIMPLE_VALIDATOR.call(valid_passport)
   end
 
   def test_can_parse_passport_file
@@ -36,70 +36,81 @@ class PassportProcessingTest < Minitest::Test
 
   def test_passport_with_invalid_birth_year
     invalid_passport = 'byr:1919 ecl:gry pid:860033327 eyr:2020 hcl:#fffffd iyr:2017 cid:147 hgt:183cm'
-    assert_equal false, PassportProcessing::COMPLETE_VALIDATOR.call(invalid_passport)
+    assert_equal false, PassportProcessing::Validator::COMPLETE_VALIDATOR.call(invalid_passport)
   end
 
   def test_passport_with_invalid_issue_year
     invalid_passport = 'iyr:2099 byr:1921 ecl:gry pid:860033327 eyr:2020 hcl:#fffffd cid:147 hgt:183cm'
-    assert_equal false, PassportProcessing::COMPLETE_VALIDATOR.call(invalid_passport)
+    assert_equal false, PassportProcessing::Validator::COMPLETE_VALIDATOR.call(invalid_passport)
   end
 
   def test_passport_with_invalid_expiration_year
     invalid_passport = 'eyr:2010 iyr:2019 byr:1921 ecl:gry pid:860033327 hcl:#fffffd cid:147 hgt:183cm'
-    assert_equal false, PassportProcessing::COMPLETE_VALIDATOR.call(invalid_passport)
+    assert_equal false, PassportProcessing::Validator::COMPLETE_VALIDATOR.call(invalid_passport)
   end
 
   def test_passport_with_invalid_height_in_cm
     invalid_passport = 'hgt:200cm eyr:2020 iyr:2019 byr:1921 ecl:gry pid:860033327 hcl:#fffffd cid:147'
-    assert_equal false, PassportProcessing::COMPLETE_VALIDATOR.call(invalid_passport)
+    assert_equal false, PassportProcessing::Validator::COMPLETE_VALIDATOR.call(invalid_passport)
   end
 
   def test_passport_with_invalid_height_in_in
     invalid_passport = 'hgt:50in eyr:2020 iyr:2019 byr:1921 ecl:gry pid:860033327 hcl:#fffffd cid:147'
-    assert_equal false, PassportProcessing::COMPLETE_VALIDATOR.call(invalid_passport)
+    assert_equal false, PassportProcessing::Validator::COMPLETE_VALIDATOR.call(invalid_passport)
   end
 
   def test_passport_with_invalid_hair_color
     invalid_passport = 'hcl:#123abz hgt:61in eyr:2020 iyr:2019 byr:1921 ecl:gry pid:860033327 cid:147'
-    assert_equal false, PassportProcessing::COMPLETE_VALIDATOR.call(invalid_passport)
+    assert_equal false, PassportProcessing::Validator::COMPLETE_VALIDATOR.call(invalid_passport)
   end
 
   def test_passport_with_invalid_eye_color
     invalid_passport = 'ecl:caz hcl:#123abc hgt:61in eyr:2020 iyr:2019 byr:1921 pid:860033327 cid:147'
-    assert_equal false, PassportProcessing::COMPLETE_VALIDATOR.call(invalid_passport)
+    assert_equal false, PassportProcessing::Validator::COMPLETE_VALIDATOR.call(invalid_passport)
   end
 
   def test_passport_with_invalid_passport_id
     invalid_passport = 'pid:86003332 ecl:blu hcl:#123abc hgt:61in eyr:2020 iyr:2019 byr:1921 cid:147'
-    assert_equal false, PassportProcessing::COMPLETE_VALIDATOR.call(invalid_passport)
+    assert_equal false, PassportProcessing::Validator::COMPLETE_VALIDATOR.call(invalid_passport)
   end
 
   def test_second_puzzle_solution
-    assert_equal 188, PassportProcessing.count_valid_passports(validator: PassportProcessing::COMPLETE_VALIDATOR)
+    assert_equal 188, PassportProcessing.count_valid_passports(
+      validator: PassportProcessing::Validator::COMPLETE_VALIDATOR
+    )
   end
 
   def test_count_valid_passport_complete_validator
     passport_filepath = './test/passport_file.txt'
-    assert_equal 2, PassportProcessing.count_valid_passports(passport_filepath: passport_filepath, validator: PassportProcessing::COMPLETE_VALIDATOR)
+    assert_equal 2, PassportProcessing.count_valid_passports(
+      passport_filepath: passport_filepath,
+      validator: PassportProcessing::Validator::COMPLETE_VALIDATOR
+    )
   end
 
   def test_count_all_invalid_passport_complete_validator
     passport_filepath = './test/invalid_passport_file.txt'
-    assert_equal 0, PassportProcessing.count_valid_passports(passport_filepath: passport_filepath, validator: PassportProcessing::COMPLETE_VALIDATOR)
+    assert_equal 0, PassportProcessing.count_valid_passports(
+      passport_filepath: passport_filepath,
+      validator: PassportProcessing::Validator::COMPLETE_VALIDATOR
+    )
   end
 
   def test_count_all_valid_passport_complete_validator
     passport_filepath = './test/valid_passport_file.txt'
-    assert_equal 4, PassportProcessing.count_valid_passports(passport_filepath: passport_filepath, validator: PassportProcessing::COMPLETE_VALIDATOR)
+    assert_equal 4, PassportProcessing.count_valid_passports(
+      passport_filepath: passport_filepath,
+      validator: PassportProcessing::Validator::COMPLETE_VALIDATOR
+    )
   end
 
   def test_height_without_scale
-    assert_equal false, PassportProcessing.validate_height('66')
+    assert_equal false, PassportProcessing::Validator.validate_height('66')
   end
 
   def test_hair_color
-    assert_equal false, PassportProcessing.validate_hair_color('#1')
-    assert PassportProcessing.validate_hair_color('#123456')
-    assert_equal false, PassportProcessing.validate_hair_color('#1234567')
+    assert_equal false, PassportProcessing::Validator.validate_hair_color('#1')
+    assert PassportProcessing::Validator.validate_hair_color('#123456')
+    assert_equal false, PassportProcessing::Validator.validate_hair_color('#1234567')
   end
 end
