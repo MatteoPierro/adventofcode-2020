ec9efa4e187f9369a69b9a5a269d50f18d249b6a
Matteo Pierro <pierromatteo@gmail.com>
2020-12-06 12:29:09 +0200

    remove duplication

--- lib/custom_customs.rb
+++ lib/custom_customs.rb
@@ -8,15 +8,11 @@ require 'set'
 module CustomCustoms
   class << self
     def sum_shared_positive_answers(file_path = nil)
-      file_path ||= './lib/custom_customs.txt'
-      groups_answers = File.read_blocks(file_path)
-      groups_answers.map(&method(:count_group_shared_positive_answers)).sum
+      sum_by_criterium(file_path: file_path, criterium: :count_group_shared_positive_answers)
     end
 
     def sum_groups_positive_answers(file_path = nil)
-      file_path ||= './lib/custom_customs.txt'
-      groups_answers = File.read_blocks(file_path)
-      groups_answers.map(&method(:count_group_positive_answers)).sum
+      sum_by_criterium(file_path: file_path, criterium: :count_group_positive_answers)
     end
 
     def count_group_positive_answers(group_answers)
@@ -25,6 +21,14 @@ module CustomCustoms
 
     def count_group_shared_positive_answers(group_answers)
       group_answers.map(&:chars).reduce(:&).count
+    end
+
+    private
+
+    def sum_by_criterium(criterium:, file_path: nil)
+      file_path ||= './lib/custom_customs.txt'
+      groups_answers = File.read_blocks(file_path)
+      groups_answers.map(&method(criterium)).sum
     end
   end
 end
