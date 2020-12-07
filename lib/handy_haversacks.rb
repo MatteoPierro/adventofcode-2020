28cb539bce5a61f71ce1d2468eb360387cf8eb88
Matteo Pierro <pierromatteo@gmail.com>
2020-12-07 21:27:36 +0200

    final refactor day7

--- lib/handy_haversacks.rb
+++ lib/handy_haversacks.rb
@@ -6,30 +6,43 @@ require 'set'
 # Solution for https://adventofcode.com/2020/day/7
 
 class HandyHaversacks
-  def self.read_bag(file_path)
-    File.readlines(file_path).each_with_object(Bags.new) do |line, bag|
-      line_match = line.match(/^(?<container>.+) bags contain (?<contents>.*)./)
-      container = line_match[:container]
-      contents = line_match[:contents].split(', ')
-      contents.each do |raw_content|
-        bag.add(container: container, raw_content: raw_content)
+  class << self
+    def read_bag(file_path)
+      File.readlines(file_path).each_with_object(Bags.new) do |line, bag|
+        line_match = line.match(/^(?<container>.+) bags contain (?<contents>.*)./)
+        container = line_match[:container]
+        contents = line_match[:contents].split(', ')
+        contents.each do |raw_content|
+          bag.add(container: container, raw_content: raw_content)
+        end
       end
     end
-  end
 
-  def self.find_shiny_gold_containers(contents_to_containers, visited = [], to_visit = contents_to_containers['shiny gold'])
-    return visited if to_visit.empty?
+    def find_shiny_gold_containers(
+      contents_to_containers,
+      visited = [],
+      to_visit = contents_to_containers['shiny gold']
+    )
+      return visited if to_visit.empty?
 
-    current_container = to_visit.pop
-    return find_shiny_gold_containers(contents_to_containers, visited, to_visit) if visited.include?(current_container)
+      current_container = to_visit.pop
+      if visited.include?(current_container)
+        return find_shiny_gold_containers(contents_to_containers, visited, to_visit)
+      end
 
-    visited << current_container
-    find_shiny_gold_containers(contents_to_containers, visited, to_visit + (contents_to_containers[current_container] || []))
-  end
+      visited << current_container
+      current_content_containers = contents_to_containers[current_container] || []
+      find_shiny_gold_containers(contents_to_containers, visited, to_visit + current_content_containers)
+    end
 
-  def self.count_bags_in_a_shiny_gold_bag(containers_to_contents, bag = 'shiny gold')
-    contents = containers_to_contents[bag]
-    contents.map { |content| content[:n] + content[:n] * count_bags_in_a_shiny_gold_bag(containers_to_contents, content[:bag]) }.sum
+    def count_bags_in_a_shiny_gold_bag(containers_to_contents, bag = 'shiny gold')
+      contents = containers_to_contents[bag]
+      contents
+        .map do |content|
+          content[:n] + content[:n] * count_bags_in_a_shiny_gold_bag(containers_to_contents, content[:bag])
+        end
+        .sum
+    end
   end
 
   class Bags
@@ -42,11 +55,20 @@ class HandyHaversacks
 
     def add(container:, raw_content:)
       content_match = raw_content.match(/^(?<size>\d+) (?<content>.*) bag/)
+      add_to_contents_to_containers(container, content_match)
+      add_to_to_containers_contents(container, content_match)
+    end
+
+    private
+
+    def add_to_contents_to_containers(container, content_match)
       content = content_match.nil? ? 'none' : content_match[:content]
       content_containers = contents_to_containers[content] || []
       content_containers << container
       contents_to_containers[content] = content_containers
+    end
 
+    def add_to_to_containers_contents(container, content_match)
       container_contents = containers_to_contents[container] || []
       container_contents << { n: content_match[:size].to_i, bag: content_match[:content] } unless content_match.nil?
       containers_to_contents[container] = container_contents
