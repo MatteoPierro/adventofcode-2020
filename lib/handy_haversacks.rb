2b1d615097713227de09e5b2ecf07cb76695b3bf
Matteo Pierro <pierromatteo@gmail.com>
2020-12-07 20:44:04 +0200

    remove not needed class

--- lib/handy_haversacks.rb
+++ lib/handy_haversacks.rb
@@ -7,25 +7,25 @@ require 'set'
 
 class HandyHaversacks
   def self.read_containers(file_path)
-    File.readlines(file_path).each_with_object({}) do |line, rules|
-      line_match = line.match(/^(?<bag>.+) bags contain (?<contents>.*)./)
-      container = line_match[:bag]
+    File.readlines(file_path).each_with_object({}) do |line, contents_to_containers|
+      line_match = line.match(/^(?<container>.+) bags contain (?<contents>.*)./)
+      container = line_match[:container]
       contents = line_match[:contents].split(', ')
       contents.each do |content|
-        content_match = content.match(/^(?<size>\d+) (?<color>.*) bag/)
-        color = content_match.nil? ? 'none' : content_match[:color]
-        containers = rules[color] || []
+        content_match = content.match(/^(?<size>\d+) (?<content>.*) bag/)
+        content = content_match.nil? ? 'none' : content_match[:content]
+        containers = contents_to_containers[content] || []
         containers << container
-        rules[color] = containers
+        contents_to_containers[content] = containers
       end
     end
   end
 
   def self.read_content(file_path)
-    File.readlines(file_path).each_with_object({}) do |line, rules|
-      line_match = line.match(/^(?<bag>.+) bags contain (?<contents>.*)./)
-      container = line_match[:bag]
-      contents = rules[container] || []
+    File.readlines(file_path).each_with_object({}) do |line, containers_to_contents|
+      line_match = line.match(/^(?<container>.+) bags contain (?<contents>.*)./)
+      container = line_match[:container]
+      contents = containers_to_contents[container] || []
       raw_contents = line_match[:contents].split(', ')
       raw_contents.each do |raw_content|
         content_match = raw_content.match(/^(?<size>\d+) (?<color>.*) bag/)
@@ -33,37 +33,22 @@ class HandyHaversacks
 
         contents << { n: content_match[:size].to_i, bag: content_match[:color] }
       end
-      rules[container] = contents
+      containers_to_contents[container] = contents
     end
   end
 
-  def self.find_shiny_gold_containers(rules)
-    new(rules).find_shiny_gold_containers
-  end
-
-  def self.find_bags_in_shiny_gold(rules)
-    contents = rules['shiny gold']
-    contents.map { |content| content[:n] + content[:n] * find_bags(rules, content[:bag]) }.sum
-  end
-
-  def self.find_bags(rules, bag)
-    contents = rules[bag]
-    contents.map { |content| content[:n] + content[:n] * find_bags(rules, content[:bag]) }.sum
-  end
-
-  attr_reader :rules
-
-  def initialize(rules)
-    @rules = rules
-  end
-
-  def find_shiny_gold_containers(visited = [], to_visit = rules['shiny gold'])
+  def self.find_shiny_gold_containers(contents_to_containers, visited = [], to_visit = contents_to_containers['shiny gold'])
     return visited if to_visit.empty?
 
-    next_container = to_visit.pop
-    return find_shiny_gold_containers(visited, to_visit) if visited.include?(next_container)
+    current_container = to_visit.pop
+    return find_shiny_gold_containers(contents_to_containers, visited, to_visit) if visited.include?(current_container)
 
-    visited << next_container
-    find_shiny_gold_containers(visited, to_visit + (rules[next_container] || []))
+    visited << current_container
+    find_shiny_gold_containers(contents_to_containers, visited, to_visit + (contents_to_containers[current_container] || []))
+  end
+
+  def self.find_bags(containers_to_contents, bag = 'shiny gold')
+    contents = containers_to_contents[bag]
+    contents.map { |content| content[:n] + content[:n] * find_bags(containers_to_contents, content[:bag]) }.sum
   end
 end
--- test/handy_haversacks_test.rb
+++ test/handy_haversacks_test.rb
@@ -5,28 +5,28 @@ require_relative '../lib/handy_haversacks'
 
 class HandyHaversacksTest < Minitest::Test
   def test_find_shiny_gold_containers
-    rules = {
+    contents_to_containers = {
       'bring white' => ['light red', 'dark orange'],
       'muted yellow' => ['light red', 'dark orange'],
       'shiny gold' => ['bring white', 'muted yellow']
     }
 
     assert_equal ['bring white', 'muted yellow', 'light red', 'dark orange'].sort,
-                 HandyHaversacks.find_shiny_gold_containers(rules).sort
+                 HandyHaversacks.find_shiny_gold_containers(contents_to_containers).sort
   end
 
   def test_first_example_find_shiny_gold_containers
-    rules = HandyHaversacks.read_containers('./test/handy_haversacks_test.txt')
-    assert_equal 4, HandyHaversacks.find_shiny_gold_containers(rules).count
+    contents_to_containers = HandyHaversacks.read_containers('./test/handy_haversacks_test.txt')
+    assert_equal 4, HandyHaversacks.find_shiny_gold_containers(contents_to_containers).count
   end
 
   def test_first_puzzle
-    rules = HandyHaversacks.read_containers('./lib/handy_haversacks.txt')
-    assert_equal 226, HandyHaversacks.find_shiny_gold_containers(rules).count
+    contents_to_containers = HandyHaversacks.read_containers('./lib/handy_haversacks.txt')
+    assert_equal 226, HandyHaversacks.find_shiny_gold_containers(contents_to_containers).count
   end
 
   def test_find_bags_in_shiny_gold
-    rules = {
+    containers_to_contents = {
       'shiny gold' => [{ n: 2, bag: 'dark red' }],
       'dark red' => [{ n: 2, bag: 'dark orange' }],
       'dark orange' => [{ n: 2, bag: 'dark yellow' }],
@@ -36,16 +36,16 @@ class HandyHaversacksTest < Minitest::Test
       'dark violet' => []
     }
 
-    assert_equal 126, HandyHaversacks.find_bags_in_shiny_gold(rules)
+    assert_equal 126, HandyHaversacks.find_bags(containers_to_contents)
   end
 
   def test_second_example_find_bags_in_shiny_gold
-    rules = HandyHaversacks.read_content('./test/handy_haversacks_test.txt')
-    assert_equal 32, HandyHaversacks.find_bags_in_shiny_gold(rules)
+    containers_to_contents = HandyHaversacks.read_content('./test/handy_haversacks_test.txt')
+    assert_equal 32, HandyHaversacks.find_bags(containers_to_contents)
   end
 
   def test_second_puzzle
-    rules = HandyHaversacks.read_content('./lib/handy_haversacks.txt')
-    assert_equal 9569, HandyHaversacks.find_bags_in_shiny_gold(rules)
+    containers_to_contents = HandyHaversacks.read_content('./lib/handy_haversacks.txt')
+    assert_equal 9569, HandyHaversacks.find_bags(containers_to_contents)
   end
 end
