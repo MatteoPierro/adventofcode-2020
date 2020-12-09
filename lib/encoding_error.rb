3bf756f724d646f63be28e0f94dbb1841ac8f5f0
Matteo Pierro <pierromatteo@gmail.com>
2020-12-09 10:38:52 +0200

    solve second puzzle day 9

--- lib/encoding_error.rb
+++ lib/encoding_error.rb
@@ -6,6 +6,25 @@ require_relative 'file_helper'
 
 class EncodingError
   class << self
+    def encryption_weakness(sequence:, target:)
+      (0...sequence.length).to_a
+                           .map { |index| sequence[index..] }
+                           .map { |s| find_limits(s, target) }
+                           .find { |l| l.length > 1 && l.sum == target }
+                           .tap do |s|
+        s.sort!
+        return s.first + s.last
+      end
+    end
+
+    def find_limits(sequence, target)
+      sum = 0
+      sequence.take_while do |value|
+        sum += value
+        sum <= target
+      end
+    end
+
     def find_error(sequence:, preamble_length: 25)
       sequence.each_cons(preamble_length + 1).find do |*preamble, number|
         combination = find_valid_combination(preamble, number)
--- test/encoding_error_test.rb
+++ test/encoding_error_test.rb
@@ -17,4 +17,25 @@ class EncodingErrorTest < Minitest::Test
     sequence = File.readlines('./lib/encoding_error.txt').map(&:to_i).to_a
     assert_equal(26_134_589, EncodingError.find_error(sequence: sequence))
   end
+
+  def test_encryption_weakness
+    sequence = [35, 20, 15, 25, 47, 40,
+                62, 55, 65, 95, 102, 117,
+                150, 182, 127, 219, 299,
+                277, 309, 576]
+
+    assert_equal(62, EncodingError.encryption_weakness(
+                       sequence: sequence,
+                       target: 127
+                     ))
+  end
+
+  def test_second_puzzle
+    sequence = File.readlines('./lib/encoding_error.txt').map(&:to_i).to_a
+    target = 26_134_589
+    assert_equal(3_535_124, EncodingError.encryption_weakness(
+                              sequence: sequence,
+                              target: target
+                            ))
+  end
 end
