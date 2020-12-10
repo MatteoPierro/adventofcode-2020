0a0ba980ea66b427ad525d5493baa6a3a12b2358
Matteo Pierro <pierromatteo@gmail.com>
2020-12-10 13:10:00 +0200

    solve second puzzle day 10

--- lib/adapter_array.rb
+++ lib/adapter_array.rb
@@ -7,10 +7,35 @@ require_relative 'file_helper'
 module AdapterArray
   class << self
     def multiply_differences(adapters)
+      differences = find_differences(adapters)
+      differences.count(1) * differences.count(3)
+    end
+
+    def find_combinations(adapters)
+      find_differences(adapters)
+        .chunk { |difference| difference != 1 }
+        .filter { |chunk| !chunk.first }
+        .map { |chunk| chunk.last.length + 1 } # +1 because you need to reach the next hop
+        .map { |sequence_length| find_sequence_combination(sequence_length) }
+        .reduce(:*)
+    end
+
+    private
+
+    def find_sequence_combination(sequence_length, current = 1)
+      return 1 if current == sequence_length
+
+      return 0 if current > sequence_length
+
+      find_sequence_combination(sequence_length, current + 1) +
+        find_sequence_combination(sequence_length, current + 2) +
+        find_sequence_combination(sequence_length, current + 3)
+    end
+
+    def find_differences(adapters)
       adapters << 0
       adapters << adapters.max + 3
-      differences = adapters.sort.each_cons(2).map { |a, b| b - a }
-      differences.count(1) * differences.count(3)
+      adapters.sort.each_cons(2).map { |a, b| b - a }
     end
   end
 end
--- test/adapter_array_test.rb
+++ test/adapter_array_test.rb
@@ -13,4 +13,14 @@ class AdapterArrayTest < Minitest::Test
     adapters = File.readlines('./lib/adapter_array.txt').map(&:to_i)
     assert_equal(2380, AdapterArray.multiply_differences(adapters))
   end
+
+  def test_find_combinations
+    adapters = [16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4]
+    assert_equal(8, AdapterArray.find_combinations(adapters))
+  end
+
+  def test_second_puzzle
+    adapters = File.readlines('./lib/adapter_array.txt').map(&:to_i) << 0
+    assert_equal(48_358_655_787_008, AdapterArray.find_combinations(adapters))
+  end
 end
