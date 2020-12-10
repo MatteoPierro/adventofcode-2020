7fc9e47f8deff77dd14a059bd394f4886a098ad3
Matteo Pierro <pierromatteo@gmail.com>
2020-12-10 13:55:48 +0200

    move to classes in order to remove duplication

--- lib/adapter_array.rb
+++ lib/adapter_array.rb
@@ -4,36 +4,40 @@ require_relative 'file_helper'
 
 # Solution for https://adventofcode.com/2020/day/10
 
-module AdapterArray
-  class << self
-    def multiply_differences(adapters)
-      adapters << 0
-      adapters << adapters.max + 3
-      find_differences_value = adapters.sort.each_cons(2).map { |a, b| b - a }
-      differences = find_differences_value
-      differences.count(1) * differences.count(3)
-    end
+class AdapterArray
+  attr_reader :adapters
 
-    # rubocop:disable Metrics/MethodLength
-    def find_combinations(adapters, target = adapters.max + 3, current = 0, memo = {})
-      return memo[current] if memo.include?(current)
-
-      if current == target
-        memo[current] = 1
-        return 1
-      end
-
-      unless adapters.include?(current)
-        memo[current] = 0
-        return 0
-      end
-
-      memo[current] = (1..3)
-                      .each
-                      .map { |inc| find_combinations(adapters, target, current + inc, memo) }
-                      .sum
-      memo[current]
-    end
-    # rubocop:enable Metrics/MethodLength
+  def initialize(adapters)
+    @adapters = adapters
+    @adapters << 0
+    @adapters << adapters.max + 3
   end
+
+  def multiply_differences
+    find_differences_value = adapters.sort.each_cons(2).map { |a, b| b - a }
+    differences = find_differences_value
+    differences.count(1) * differences.count(3)
+  end
+
+  # rubocop:disable Metrics/MethodLength
+  def count_combinations(target = adapters.max + 3, current = 0, memo = {})
+    return memo[current] if memo.include?(current)
+
+    if current == target
+      memo[current] = 1
+      return 1
+    end
+
+    unless adapters.include?(current)
+      memo[current] = 0
+      return 0
+    end
+
+    memo[current] = (1..3)
+                    .each
+                    .map { |inc| count_combinations(target, current + inc, memo) }
+                    .sum
+    memo[current]
+  end
+  # rubocop:enable Metrics/MethodLength
 end
--- test/adapter_array_test.rb
+++ test/adapter_array_test.rb
@@ -6,25 +6,33 @@ require_relative '../lib/adapter_array'
 class AdapterArrayTest < Minitest::Test
   def test_multiply_differences
     adapters = [16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4]
-    assert_equal(35, AdapterArray.multiply_differences(adapters))
+
+    adapter_array = AdapterArray.new(adapters)
+
+    assert_equal(35, adapter_array.multiply_differences)
   end
 
   def test_first_puzzle
     adapters = File.readlines('./lib/adapter_array.txt').map(&:to_i)
-    assert_equal(2380, AdapterArray.multiply_differences(adapters))
+
+    adapter_array = AdapterArray.new(adapters)
+
+    assert_equal(2380, adapter_array.multiply_differences)
   end
 
   def test_find_combinations
     adapters = [16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4]
-    # puts adapters.sort
-    adapters << 0
-    adapters << adapters.max + 3
-    # puts adapters.sort
-    assert_equal(8, AdapterArray.find_combinations(adapters))
+
+    adapter_array = AdapterArray.new(adapters)
+
+    assert_equal(8, adapter_array.count_combinations)
   end
 
   def test_second_puzzle
-    adapters = File.readlines('./lib/adapter_array.txt').map(&:to_i) << 0
-    assert_equal(48_358_655_787_008, AdapterArray.find_combinations(adapters))
+    adapters = File.readlines('./lib/adapter_array.txt').map(&:to_i)
+
+    adapter_array = AdapterArray.new(adapters)
+
+    assert_equal(48_358_655_787_008, adapter_array.count_combinations)
   end
 end
