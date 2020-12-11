5058237c1835d22b9a3bfb3062c85e1631a1bdeb
Matteo Pierro <pierromatteo@gmail.com>
2020-12-11 19:39:17 +0200

    remove not used test

--- test/seating_system_test.rb
+++ test/seating_system_test.rb
@@ -18,43 +18,6 @@ class SeatingSystemTest < Minitest::Test
     assert_equal(2299, SeatingSystem.count_stabilized_empty_seat('./lib/seating_system.txt'))
   end
 
-  def test_x
-    skip
-    layout = ["L.......#....",
-              ".L.L.#.#.#.#.",
-              ".............",
-              '..#..........']
-
-    seating_system = SeatingSystem.new(layout.map(&:chars))
-    # assert_equal(layout.join("\n"), seating_system.to_s)
-
-    row = layout[1]
-
-    # row_index = 1
-    # seat_index = 1
-    # assert_equal('.', seating_system.find_neighbours_2(row, row_index, seat_index))
-
-    row_index = 0
-    seat_index = 2
-    assert_equal('L', seating_system.find_neighbour([1,1],row, row_index, seat_index))
-    assert_equal('L', seating_system.find_neighbour([-1,1],row, row_index, seat_index))
-    assert_equal('#', seating_system.find_neighbour([0,1],row, row_index, seat_index))
-    assert_equal('L', seating_system.find_neighbour([-1,0],row, row_index, seat_index))
-    assert_equal('#', seating_system.find_neighbour([1,0],row, row_index, seat_index))
-
-    row_index = 3
-    seat_index = 8
-    assert_equal('#', seating_system.find_neighbour([0,-1],row, row_index, seat_index))
-
-    row_index = 3
-    seat_index = 9
-    assert_equal('#', seating_system.find_neighbour([-1,-1],row, row_index, seat_index))
-
-    row_index = 3
-    seat_index = 3
-    assert_equal('#', seating_system.find_neighbour([1,-1],row, row_index, seat_index))
-  end
-
   def test_count_occupied_seats_extended
     seating_system = SeatingSystem.from_file(
       './test/seating_system_test.txt',
@@ -71,9 +34,9 @@ class SeatingSystemTest < Minitest::Test
   def test_second_puzzle
     # skip # Quite slow, enable it only when needed
     assert_equal(2047, SeatingSystem.count_stabilized_empty_seat(
-      './lib/seating_system.txt',
-      occupied_neighbours: 5,
-      neighbours_finder: SeatingSystem::EXTENDED_NEIGHBOUR_FINDER
-      ))
+                         './lib/seating_system.txt',
+                         occupied_neighbours: 5,
+                         neighbours_finder: SeatingSystem::EXTENDED_NEIGHBOUR_FINDER
+                       ))
   end
 end
