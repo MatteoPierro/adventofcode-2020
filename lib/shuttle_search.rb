d7d87d000a0932d720fb469f01cbc6fc70cc95e8
Matteo Pierro <pierromatteo@gmail.com>
2020-12-13 13:04:56 +0200

    day 13 save point

--- lib/shuttle_search.rb
+++ lib/shuttle_search.rb
@@ -19,5 +19,23 @@ class ShuttleSearch
       .min { |b1, b2| b1.time <=> b2.time }
   end
 
+  def self.find(raw_busses)
+    busses =
+      raw_busses.reverse
+        .map.with_index
+        .reject { |s, _| s == 'x' }
+        .map { |s, p| [s.to_i, p] }
+        .to_a
+
+    pace = busses.first.first
+    maximum = busses.map { |b| b.first }.reduce(&:*)
+
+    loop do
+      break if busses.all? { |bus_id, position| (maximum - position) % bus_id == 0}
+      maximum -= pace
+    end
+    maximum - busses.last.last
+  end
+
   Departure = Struct.new(:bus, :time)
 end
--- test/shuttle_search_test.rb
+++ test/shuttle_search_test.rb
@@ -18,4 +18,17 @@ class ShuttleSearchTest < Minitest::Test
   def test_first_puzzle
     assert_equal(2845, ShuttleSearch.minutes_to_wait)
   end
+
+  def test_something
+    assert_equal(754018, ShuttleSearch.find(%w[67 7 59 61]))
+    assert_equal(779210, ShuttleSearch.find(%w[67 x 7 59 61]))
+    assert_equal(1068781, ShuttleSearch.find(%w[7 13 x x 59 x 31 19]))
+  end
+
+  def test_second_puzzle
+    # raw_busses = File.readlines('./lib/shuttle_search.txt')[1].split(',')
+    assert_equal(325, ShuttleSearch.find(%w[13 x x 41]))
+    assert_equal(92168, ShuttleSearch.find(%w[328 x x x x x x x x x 569]))
+    assert_equal(92168, ShuttleSearch.find(%w[13 x x 41 x x x x x x x x x 569]))
+  end
 end
