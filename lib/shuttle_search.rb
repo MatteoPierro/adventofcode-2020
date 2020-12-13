943ed342b7a1123df03f53c36a791b45e9beca39
Matteo Pierro <pierromatteo@gmail.com>
2020-12-13 15:56:50 +0200

    solve second puzzle day 13

--- lib/shuttle_search.rb
+++ lib/shuttle_search.rb
@@ -19,23 +19,35 @@ class ShuttleSearch
       .min { |b1, b2| b1.time <=> b2.time }
   end
 
-  def self.find(raw_busses)
-    busses =
-      raw_busses.reverse
-        .map.with_index
-        .reject { |s, _| s == 'x' }
-        .map { |s, p| [s.to_i, p] }
-        .to_a
+  def self.find_consecutive_time(raw_busses)
+    busses = busses_sorted_by_number(raw_busses)
+    maximum_time = busses.map(&:number).reduce(&:*)
+    (highest, *remaining) = busses
+    pace = highest.number
+    current_time = maximum_time - highest.position
 
-    pace = busses.first.first
-    maximum = busses.map { |b| b.first }.reduce(&:*)
+    remaining.each do |current_bus|
+      current_time -= pace until current_bus.correct_position?(current_time)
 
-    loop do
-      break if busses.all? { |bus_id, position| (maximum - position) % bus_id == 0}
-      maximum -= pace
+      pace *= current_bus.number
     end
-    maximum - busses.last.last
+
+    current_time
   end
 
   Departure = Struct.new(:bus, :time)
+  Bus = Struct.new(:number, :position) do
+    def correct_position?(current_time)
+      ((current_time + position) % number).zero?
+    end
+  end
+
+  def self.busses_sorted_by_number(raw_busses)
+    raw_busses
+      .map.with_index
+      .reject { |s, _| s == 'x' }
+      .map { |number, position| Bus.new(number.to_i, position) }
+      .sort { |b1, b2| b2.number <=> b1.number }
+      .to_a
+  end
 end
