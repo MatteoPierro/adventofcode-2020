daa3a031ea208bd4cf52235ac586f261849f4865
Matteo Pierro <pierromatteo@gmail.com>
2020-12-16 11:13:26 +0200

    clean up code

--- lib/ticket_translation.rb
+++ lib/ticket_translation.rb
@@ -5,10 +5,62 @@ require_relative 'file_helper'
 # Solution for https://adventofcode.com/2020/day/16
 
 class TicketTranslation
+  class << self
+    def from_file(file_path)
+      notes_blocks = File.read_blocks(file_path)
+      ticket_limits = parse_ticket_limits(notes_blocks[0])
+      nearby_tickets = parse_nearby_tickets(notes_blocks[-1][1..])
+      new(nearby_tickets: nearby_tickets, ticket_limits: ticket_limits)
+    end
 
-  Limit = Struct.new(:min, :max) do
+    private
+
+    def parse_ticket_limits(raw_limits)
+      ticket_limits = {}
+      raw_limits.each do |row_limit|
+        limit_name, limits = parse_field_limits(row_limit)
+        ticket_limits[limit_name] = limits
+      end
+      ticket_limits
+    end
+
+    def parse_field_limits(row_limit)
+      limit_name = row_limit.match(/^(.*):/)[1]
+      limits = row_limit.scan(/\s(\d+)-(\d+)/)
+                        .map { |l| TicketTranslation::Limit.new(l.first.to_i, l.last.to_i) }
+                        .to_a
+      [limit_name, limits]
+    end
+
+    def parse_nearby_tickets(raw_nearby_tickets)
+      raw_nearby_tickets
+        .map { |raw_ticket| raw_ticket.split(',').map(&:to_i) }
+        .to_a
+    end
+  end
+
+  attr_reader :ticket_limits, :nearby_tickets
+
+  def initialize(nearby_tickets:, ticket_limits:)
+    @ticket_limits = ticket_limits
+    @nearby_tickets = nearby_tickets
+  end
+
+  def sum_invalid_field_in_nearby_tickets
+    nearby_tickets.flatten.reject(&method(:any_limit_match?)).sum
+  end
+
+  Limit = Struct.new(:min_value, :max_value) do
     def include?(value)
-      value >= min && value <= max
+      value >= min_value && value <= max_value
+    end
+  end
+
+  private
+
+  def any_limit_match?(value)
+    ticket_limits.any? do |_, field_limits|
+      field_limits.any? { |limit| limit.include?(value) }
     end
   end
 end
--- test/ticket_translation_test.rb
+++ test/ticket_translation_test.rb
@@ -4,26 +4,9 @@ require 'minitest/autorun'
 require_relative '../lib/ticket_translation'
 
 class TicketTranslationTest < Minitest::Test
+  def test_first_puzzle
+    ticket_translation = TicketTranslation.from_file('./lib/ticket_translation.txt')
 
-  def test_read_notes
-    notes_blocks = File.read_blocks('./lib/ticket_translation.txt')
-    raw_limits = notes_blocks[0]
-    nearby_tickets_values = notes_blocks[-1][1..-1]
-                           .map{ |raw_ticket| raw_ticket.split(',').map(&:to_i) }
-                           .flatten
-                           .to_a
-    ticket_limits = raw_limits
-             .map { |raw_field_limits| raw_field_limits.scan(/\s(\d+)-(\d+)/) }
-             .map { |fields_limits| fields_limits.map { |l| TicketTranslation::Limit.new(l.first.to_i, l.last.to_i)} }
-             .flatten
-             .to_a
-
-    result = nearby_tickets_values.reject do |value|
-      ticket_limits.any? do |limit|
-        limit.include?(value)
-      end
-    end.sum
-
-    assert_equal(19093, result)
+    assert_equal(19_093, ticket_translation.sum_invalid_field_in_nearby_tickets)
   end
 end
