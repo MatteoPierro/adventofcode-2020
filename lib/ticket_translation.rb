5e102b389b2fd9b51fa8fc24bd617bdb24f94eea
Matteo Pierro <pierromatteo@gmail.com>
2020-12-16 18:24:12 +0200

    solve second puzzle day 16

--- lib/ticket_translation.rb
+++ lib/ticket_translation.rb
@@ -9,8 +9,9 @@ class TicketTranslation
     def from_file(file_path)
       notes_blocks = File.read_blocks(file_path)
       ticket_limits = parse_ticket_limits(notes_blocks[0])
+      ticket = parse_ticket(notes_blocks[1][1])
       nearby_tickets = parse_nearby_tickets(notes_blocks[-1][1..])
-      new(nearby_tickets: nearby_tickets, ticket_limits: ticket_limits)
+      new(nearby_tickets: nearby_tickets, ticket_limits: ticket_limits, ticket: ticket)
     end
 
     private
@@ -34,20 +35,64 @@ class TicketTranslation
 
     def parse_nearby_tickets(raw_nearby_tickets)
       raw_nearby_tickets
-        .map { |raw_ticket| raw_ticket.split(',').map(&:to_i) }
+        .map(&method(:parse_ticket))
         .to_a
+    end
+
+    def parse_ticket(raw_ticket)
+      raw_ticket.split(',').map(&:to_i)
     end
   end
 
-  attr_reader :ticket_limits, :nearby_tickets
+  attr_reader :ticket_limits, :nearby_tickets, :ticket
 
-  def initialize(nearby_tickets:, ticket_limits:)
+  def initialize(nearby_tickets:, ticket_limits:, ticket:)
     @ticket_limits = ticket_limits
     @nearby_tickets = nearby_tickets
+    @ticket = ticket
   end
 
   def sum_invalid_field_in_nearby_tickets
     nearby_tickets.flatten.reject(&method(:any_limit_match?)).sum
+  end
+
+  def multiply_departure_fields
+    find_rules_position.each.with_index
+                       .select { |name, _| name.start_with?('departure') }
+                       .map { |_, index| ticket[index] }
+                       .reduce(:*)
+  end
+
+  def valid_nearby_tickets
+    nearby_tickets.filter do |nearby_ticket|
+      nearby_ticket.all? { |value| any_limit_match?(value) }
+    end
+  end
+
+  def find_rules_position
+    rules = initial_guessing
+
+    until rules.all? { |rule| rule.length == 1 }
+      found_rules = rules.select { |rule| rule.length == 1 }.flatten.to_a
+      rules.each.with_index do |rule, index|
+        next if rule.length == 1
+
+        rules[index] = rule - found_rules
+      end
+    end
+
+    rules.flatten
+  end
+
+  def possible_rules(valid_ticket)
+    valid_ticket.map(&method(:value_rules))
+  end
+
+  def value_rules(value)
+    ticket_limits
+      .select { |_, limits| limits.any? { |l| l.include?(value) } }
+      .map { |name, _| name }
+      .to_a
   end
 
   Limit = Struct.new(:min_value, :max_value) do
@@ -57,6 +102,24 @@ class TicketTranslation
   end
 
   private
+
+  def initial_guessing
+    rules = possible_tickets_rules.first
+
+    possible_tickets_rules.each do |possible_rules|
+      possible_rules.each_with_index do |possible_rule, index|
+        rules[index] &= possible_rule
+      end
+    end
+
+    rules
+  end
+
+  def possible_tickets_rules
+    valid_nearby_tickets
+      .map { |ticket| possible_rules(ticket) }
+      .to_a
+  end
 
   def any_limit_match?(value)
     ticket_limits.any? do |_, field_limits|
--- test/ticket_translation_test.rb
+++ test/ticket_translation_test.rb
@@ -9,4 +9,35 @@ class TicketTranslationTest < Minitest::Test
 
     assert_equal(19_093, ticket_translation.sum_invalid_field_in_nearby_tickets)
   end
+
+  def test_valid_nearby_tickets
+    ticket_translation = TicketTranslation.from_file('./test/ticket_translation_test.txt')
+
+    assert_equal([[7, 3, 47]], ticket_translation.valid_nearby_tickets)
+  end
+
+  # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
+  def test_find_rules_position
+    ticket_translation = TicketTranslation.from_file('./test/ticket_translation_test_2.txt')
+
+    first = ticket_translation.possible_rules([3, 9, 18])
+    second = ticket_translation.possible_rules([15, 1, 5])
+    third = ticket_translation.possible_rules([5, 14, 9])
+    assert_equal([%w[row seat], %w[class row seat], %w[class row seat]], first)
+    assert_equal([%w[class row], %w[class row seat], %w[class row seat]], second)
+    assert_equal([%w[class row seat], %w[class row], %w[class row seat]], third)
+
+    assert_equal(['row'], first.first & second.first & third.first)
+    assert_equal(%w[class row], first[1] & second[1] & third[1])
+    assert_equal(%w[class row seat], first[2] & second[2] & third[2])
+
+    assert_equal(%w[row class seat], ticket_translation.find_rules_position)
+  end
+  # rubocop:enable Metrics/MethodLength, Metrics/AbcSize
+
+  def test_second_puzzle
+    ticket_translation = TicketTranslation.from_file('./lib/ticket_translation.txt')
+
+    assert_equal(5_311_123_569_883, ticket_translation.multiply_departure_fields)
+  end
 end
